cc42a770c4b5c582cd9303f203e218aa9f73b305
matte <matteomanni1999@gmail.com>
2025-08-09 11:33:25 +0200

    collisions  impl

--- box.py
+++ box.py
@@ -1,24 +1,38 @@
 
 class Box:
     def __init__(self, canvas, x, y, config):
-        self.x = x
-        self.y = y
-        self.dx = 0
-        self.dy = 0
         self.width = config["box"]["width"]
         self.height = config["box"]["height"]
-        self.corners = [(x, y), (x + self.width, y), (x + self.width, y + self.height), (x, y + self.height)]
-
+        self.corners = [x, y, x + self.width, y + self.height]
         self.canvas = canvas
         self.box = canvas.create_rectangle(
-            x, y,
-            x + self.width,
-            y + self.height,
+            self.corners[0],
+            self.corners[1],
+            self.corners[2],
+            self.corners[3],
             fill=config["box"]["color"]
         )
+        self.speed = config["box"]["speed"]
     
-    def move(self):
-        self.x += self.dx
-        self.y += self.dy
-        self.canvas.coords(self.box, self.x, self.y, self.x + self.width, self.y + self.height)
+    def move(self, direction):
+        if direction == "right":
+            self.corners[0] += self.speed
+            self.corners[2] += self.speed
+        elif direction == "left":
+            self.corners[0] -= self.speed
+            self.corners[2] -= self.speed
+        elif direction == "up":
+            self.corners[1] -= self.speed
+            self.corners[3] -= self.speed
+        elif direction == "down":
+            self.corners[1] += self.speed
+            self.corners[3] += self.speed
     
+    def update(self):
+        self.canvas.coords(
+            self.box,
+            self.corners[0],
+            self.corners[1],
+            self.corners[2],
+            self.corners[3]
+        )
--- screen.py
+++ screen.py
@@ -15,7 +15,6 @@ class MyScreen:
         self.box_index = 0
         self.box_list = []
         for i in range(self.config["num_boxes"]):
-            print("canva type", type(self.canvas))
             self.box_list.append(Box(self.canvas, i*100, i*10, self.config))
 
         self.root.bind("<KeyPress>", self.key_press)
@@ -23,85 +22,75 @@ class MyScreen:
 
         self.update()
 
+
     def key_press(self, event):
         key = event.keysym.lower()
-        
-        speed = self.config["speed"]
-    
-        if key == "d":
-            self.box_list[self.box_index].dx = speed
-        elif key == "a":
-            self.box_list[self.box_index].dx = -speed
-        elif key == "s":
-            self.box_list[self.box_index].dy = speed
-        elif key == "w":
-            self.box_list[self.box_index].dy = -speed
 
-        restrictions = self.check_collisions()
-        print("restrictions", restrictions)
-        if "right" in restrictions or "left" in restrictions:
-            self.box_list[self.box_index].dx = 0
-        if "up" in restrictions or "down" in restrictions:
-            self.box_list[self.box_index].dy = 0
         if key == "1": self.box_index = 0  # Select first box
         elif key == "2": self.box_index = 1  # Select second box
         elif key == "3": self.box_index = 2  # Select third box
         elif key == "escape": self.root.quit()  # Exit on Escape key
 
+        if key == "d":
+            if self.check_collisions("right") is False:
+                self.box_list[self.box_index].move("right")
+        elif key == "a":
+            if self.check_collisions("left") is False:
+                self.box_list[self.box_index].move("left")
+        elif key == "s":
+            if self.check_collisions("down") is False:
+                self.box_list[self.box_index].move("down")
+        elif key == "w":
+            if self.check_collisions("up") is False:
+                self.box_list[self.box_index].move("up")
+
+        
     def key_release(self, event):
         key = event.keysym.lower()
-        if key in ["a", "d"]: self.box_list[self.box_index].dx = 0
-        if key in ["w", "s"]: self.box_list[self.box_index].dy = 0
-    
-    # def key_press(self, event):
-    #     if event.keysym == "Right": self.dx = 5
-    #     elif event.keysym == "Left": self.dx = -5
-    #     elif event.keysym == "Down": self.dy = 5
-    #     elif event.keysym == "Up": self.dy = -5
-
-    # def key_release(self, event):
-    #     if event.keysym in ["Right", "Left"]: self.dx = 0
-    #     if event.keysym in ["Up", "Down"]: self.dy = 0
+        # if key in ["a", "d"]: self.box_list[self.box_index].dx = 0
+        # if key in ["w", "s"]: self.box_list[self.box_index].dy = 0
 
     def update(self):
         for box in self.box_list:
-            box.move()
-        
+            box.update()
+
         self.root.after(16, self.update) # ~60 FPS
 
     def run(self):
         self.root.mainloop()
 
-    def check_collisions(self):
-        restrictions = []
+    def check_collisions(self, direction):
+        collision = False
         for i in range(len(self.box_list)):
             if i != self.box_index:
-                restrictions += self.check_collision(self.box_list[self.box_index], self.box_list[i])
+                collision |= self.check_collision(self.box_list[self.box_index], self.box_list[i], direction)
+                if collision: break
 
-        return restrictions
+        return collision
     
-    def check_collision(self, box1, box2):
-        restrictions = []
-        #futura collisione a destra
-        if (box1.corners[0][0] + box1.dx + box1.width >= box2.corners[0][0] and
-            box1.corners[0][1] + box1.dy <= box2.corners[0][1] + box2.height and
-            box1.corners[0][1] + box1.dy + box1.height >= box2.corners[0][1]):
-            restrictions.append("right")
-        #futura collisione a sinistra
-        if (box1.corners[0][0] + box1.dx <= box2.corners[0][0] + box2.width and
-            box1.corners[0][1] + box1.dy <= box2.corners[0][1] + box2.height and
-            box1.corners[0][1] + box1.dy + box1.height >= box2.corners[0][1]):
-            restrictions.append("left")
-        #futura collisione in alto
-        if (box1.corners[0][0] + box1.dx <= box2.corners[0][0] + box2.width and
-            box1.corners[0][1] + box1.dy <= box2.corners[0][1] + box2.height and
-            box1.corners[0][1] + box1.dy + box1.height >= box2.corners[0][1]):
-            restrictions.append("up")
-        #futura collisione in basso
-        if (box1.corners[0][0] + box1.dx + box1.width >= box2.corners[0][0] and
-            box1.corners[0][1] + box1.dy <= box2.corners[0][1] + box2.height and
-            box1.corners[0][1] + box1.dy + box1.height >= box2.corners[0][1]):
-            restrictions.append("down")
-            
-        return restrictions
-    
+    def check_collision(self, box1, box2, direction):
+        if direction == "up":
+            return ((box2.corners[0] < box1.corners[0] and box1.corners[0] < box2.corners[2]) or \
+                    (box2.corners[0] < box1.corners[2] and box1.corners[2] < box2.corners[2]) or\
+                    (box2.corners[0] == box1.corners[0] and box1.corners[2] == box2.corners[2])) and \
+                    box1.corners[1] - box1.speed < box2.corners[3] and \
+                    box1.corners[3] > box2.corners[1]
+        elif direction == "down":
+            return ((box2.corners[0] < box1.corners[0] and box1.corners[0] < box2.corners[2]) or \
+                    (box2.corners[0] < box1.corners[2] and box1.corners[2] < box2.corners[2]) or \
+                    (box2.corners[0] == box1.corners[0] and box1.corners[2] == box2.corners[2])) and \
+                    box1.corners[3] + box1.speed > box2.corners[1] and \
+                    box1.corners[1] < box2.corners[3]
+        elif direction == "left":
+            return ((box2.corners[1] < box1.corners[1] and box1.corners[1] < box2.corners[3]) or \
+                    (box2.corners[1] < box1.corners[3] and box1.corners[3] < box2.corners[3]) or \
+                    (box2.corners[1] == box1.corners[1] and box1.corners[3] == box2.corners[3])) and \
+                    box1.corners[0] - box1.speed < box2.corners[2] and \
+                    box1.corners[2] > box2.corners[0]
+        elif direction == "right":
+            return ((box2.corners[1] < box1.corners[1] and box1.corners[1] < box2.corners[3]) or \
+                    (box2.corners[1] < box1.corners[3] and box1.corners[3] < box2.corners[3]) or \
+                    (box2.corners[1] == box1.corners[1] and box1.corners[3] == box2.corners[3])) and \
+                    box1.corners[2] + box1.speed > box2.corners[0] and \
+                    box1.corners[0] < box2.corners[2]
+        return False
